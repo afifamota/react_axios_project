329b9afed92c48d68cf7fc6bcb8616373d76b5e5
afra <afra@gmail.com>
2024-03-01 12:25:45 +0530

    changing the code

--- axios/src/App.jsx
+++ axios/src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import axios from 'axios';
+import { Table } from 'react-bootstrap';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 import FetchData  from './FetchData'
 function App() {
+ 
   
   return (
+    <div className='App'>
+        < FetchData/>
+        <Table/> 
+       
+
+    </div>
     
-       < FetchData/> 
+      
   )
 }
 
-export default App
+export default App;
--- axios/src/FetchData.jsx
+++ axios/src/FetchData.jsx
@@ -1,45 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Table } from 'react-bootstrap';
 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
-function FetchData(){
-    const [data, setData] = useState([])
-    useEffect(() => {
-        axios.get("https://jsonplaceholder.typicode.com/posts")
-        .then(res => setData(res.data))
-        .catch(err => console.log(err));
-    },[]);
-    return(
-        <div className='container'>
-            <div className='mt-3'>;
-                <h3>Fetch Data from API in React with Axios</h3>
-                <table className='table'>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Title</th>
-                            <th>Body</th>
-                            {/* <th>City</th> */}
-                        </tr>
-                    </thead>
-                    <tbody>{
-                        
-                        data.map((user,index) => {
-                            return <tr key={index}>
-                              <td>{user.id}</td> 
-                              <td>{user.title}</td>
-                              <td>{user.body}</td>
-                              {/* <td>{user.address.city}</td>  */}
-                            </tr>
-                        })}
+function FetchData() {
+  const [data, setData] = useState([]);
 
-                    </tbody>
-                </table>
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(res => setData(res.data))
+      .catch(err => console.log(err));
+  }, []);
 
-            </div>
+  return (
+    <div className='container'>
+      <div className='mt-3'>
+        <h3>POST LIST</h3>
 
-        </div>
-    )
+        <Table striped bordered hover >
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Title</th>
+              <th>Body</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((user, index) => (
+              <tr key={index}>
+                <td>{user.id}</td>
+                <td>{user.title}</td>
+                <td>{user.body}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  );
 }
-export default FetchData
+
+export default FetchData;
